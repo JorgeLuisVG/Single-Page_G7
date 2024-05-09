e02fadfa2c941d85b9131996893e26d97eb6bf66
JorgeLuisVG <158310235+JorgeLuisVG@users.noreply.github.com>
2024-05-09 08:41:07 -0600

    archivo pyton
    se agrego el archivo main.py con los datos de los estudiantes

---
+++ main.py
@@ -0,0 +1,8 @@
+print ("Jorge Luis Villagrán González")
+print ("1523624")
+
+print ("Alexander Dumarsell De Leon Flores")
+print ("1517924")
+
+print("David Alberto Ordoñez Hernández ")
+print("1529924")
