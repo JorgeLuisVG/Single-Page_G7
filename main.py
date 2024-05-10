8be3f0071ca8deec313dc7e70ae62d947ab419b4
JorgeLuisVG <158310235+JorgeLuisVG@users.noreply.github.com>
2024-05-10 10:03:19 -0600

    se eliminaron archivos para la prueba

--- main.py
+++
@@ -1,8 +0,0 @@
-print ("Jorge Luis Villagrán González")
-print ("1523624")
-
-print ("Alexander Dumarsell De Leon Flores")
-print ("1517924")
-
-print("David Alberto Ordoñez Hernández ")
-print("1529924")
